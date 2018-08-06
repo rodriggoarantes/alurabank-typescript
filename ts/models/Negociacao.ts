2e91eaf6681e0887a8d17a707f3b3c763c200680
Rodrigo Arantes <rodrigo.arantes@pcinformatica.com.br>
2018-08-06 13:04:03 -0300

    Atualizando projeto

--- ts/models/Negociacao.ts
+++ ts/models/Negociacao.ts
@@ -1,27 +1,24 @@
 class Negociacao {
-	private _data: Date;
-	private _quantidade: number;
-	private _valor: number;
+    
+    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
 
-	constructor(data: Date, quantidade: number, valor: number) {
-		this._data = data;
-		this._quantidade = quantidade;
-		this._valor = valor;
-	}
+    get data() {
 
-	get data() {
-		return this._data;
-	}
+        return this._data;
+    }
 
-	get quantidade() {
-		return this._quantidade;
-	}
+    get quantidade() {
 
-	get valor() {
-		return this._valor;
-	}
+        return this._quantidade;
+    }
 
-	get volume() {
-		return this._quantidade * this._valor;
-	}
+    get valor() {
+
+        return this._valor;
+    }
+
+    get volume() {
+
+        return this._quantidade * this._valor;
+    }
 }
